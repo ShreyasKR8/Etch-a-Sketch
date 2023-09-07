4d2e6469c0e19fa83c0dfcbd6d14925f66bac326
Shreyaskr8 <skrravi08@gmail.com>
2023-09-07 22:35:54 +0530

    Add a button to fill random colors on each interaction

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
-let gridSize = prompt("Enter grid size");
 const containerDiv = document.querySelector('.grid-container');
-// console.log(containerDiv);
 let gridDiv;
-function fillGrids()
+let mouseDown = false;
+let isRandomRGB = false;
+let chosenColor = 'rgb(0, 0, 0)';
+
+function fillGrids(gridSize = 16)
 {
     containerDiv.innerHTML = ''; // clears previous divs, if any
     for(let i = 0; i < gridSize*gridSize; i++)
@@ -15,14 +17,69 @@ function fillGrids()
     }
 }
 
+function applyColorOnClick(e)
+{
+    if(e.type === 'mouseover' && !mouseDown)
+    {
+        return;
+    }
+    const target = e.target;
+    target.style.backgroundColor = chosenColor; 
+}
+
+function applyColorOnHover(e)
+{
+    if(isRandomRGB)
+    {
+        setRandomColor();
+    }
+    const target = e.target;
+    target.style.backgroundColor = chosenColor; 
+}
+
+function setRandomColor()
+{
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    chosenColor = `rgb(${r}, ${g}, ${b})`;
+}
+
 fillGrids();
+
+// containerDiv.addEventListener('mouseover', applyColorOnClick);
+// containerDiv.addEventListener('mousedown', applyColorOnClick);
+containerDiv.addEventListener('mouseover', applyColorOnHover);
+
+document.body.onmousedown =  () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
 const gridSizeBtn = document.querySelector('.grid-size-button');
 gridSizeBtn.addEventListener('click', () => {
-    gridSize = prompt("Enter grid size");
-    fillGrids();
-})
+    do{
+        var gridSize = prompt("Enter the grid size");
+    }while(gridSize > 100 || gridSize <= 0);
+    fillGrids(gridSize);
+});
 
-containerDiv.addEventListener('mouseover', (e) => {
-    const target = e.target;
-    target.style.backgroundColor = 'black';
+const randomColorBtn = document.querySelector('.random-rgb-button');
+randomColorBtn.addEventListener('click', () => {
+    if(!isRandomRGB)
+    {
+        isRandomRGB = true;
+        randomColorBtn.textContent = 'Black Color';
+    }
+    else 
+    {
+        isRandomRGB = false;
+        chosenColor = 'rgb(0, 0, 0)';
+        randomColorBtn.textContent = 'Random RGB';
+    }
+});
+
+const clearBtn = document.querySelector('.clear-button');
+clearBtn.addEventListener('click', () => {
+    containerDiv.childNodes.forEach(gridDiv => {
+        gridDiv.style.backgroundColor = 'white'
+    });
 })
