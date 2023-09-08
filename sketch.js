d349dc3147b0a4a7855b68e8622e86aa0400966e
Shreyaskr8 <skrravi08@gmail.com>
2023-09-08 17:15:30 +0530

    add erase functionality

--- sketch.js
+++ sketch.js
@@ -2,16 +2,18 @@ let gridDiv;
 let mouseDown = false;
 let isRandomRGB = false;
 let chosenColor = 'rgb(0, 0, 0)';
+let isEraseMode = false;
 const containerDiv = document.querySelector('.grid-container');
 const gridSizeBtn = document.querySelector('.grid-size-button');
 const randomColorBtn = document.querySelector('.random-rgb-button');
 const toggleGridBtn = document.querySelector('.toggle-grid-button');
+const eraseBtn = document.querySelector('.erase-button');
 const clearBtn = document.querySelector('.clear-button');
 
 function fillGrids(gridSize = 16)
 {
     containerDiv.innerHTML = ''; // clears previous divs, if any
-    for(let i = 0; i < gridSize*gridSize; i++)
+    for(let i = 0; i < gridSize * gridSize; i++)
     {
         gridDiv = document.createElement('div');
         gridDiv.classList.add('grid');
@@ -21,23 +23,28 @@ function fillGrids(gridSize = 16)
     }
 }
 
-function applyColorOnClick(e)
+function applyColorOnClick(event)
 {
-    if(e.type === 'mouseover' && !mouseDown)
+    if(event.type === 'mouseover' && !mouseDown)
     {
         return;
     }
-    const target = e.target;
+    const target = event.target;
     target.style.backgroundColor = chosenColor; 
 }
 
-function applyColorOnHover(e)
+function applyColorOnHover(event)
 {
+    if(isEraseMode)
+    {
+        eraseColor(event);
+        return;
+    }
     if(isRandomRGB)
     {
         setRandomColor();
     }
-    const target = e.target;
+    const target = event.target;
     target.style.backgroundColor = chosenColor; 
 }
 
@@ -49,13 +56,19 @@ function setRandomColor()
     chosenColor = `rgb(${r}, ${g}, ${b})`;
 }
 
-function toggleGridLines(gridLines)
+function toggleGridLines()
 {
     containerDiv.classList.toggle('remove-grid-border');
     
     containerDiv.childNodes.forEach(gridDiv => {
         gridDiv.classList.toggle('remove-grid-border');
     });
+}
+
+function eraseColor(eventHover)
+{
+    const targetDiv = eventHover.target;
+    targetDiv.style.backgroundColor = 'white';
 }
 
 function clearGrid()
@@ -96,5 +109,21 @@ randomColorBtn.addEventListener('click', () => {
 });
 
 toggleGridBtn.addEventListener('click', toggleGridLines);
+
+eraseBtn.addEventListener('click', (event) => {
+    if(!isEraseMode)
+    {
+        eraseBtn.style.backgroundColor = 'white';
+        eraseBtn.style.color = 'black';
+        isEraseMode = true;
+    }
+    else 
+    {
+        isEraseMode = false;
+        eraseBtn.style.backgroundColor = 'black';
+        eraseBtn.style.color = 'white';
+        chosenColor = 'rgb(0, 0, 0)';
+    }
+});
 
 clearBtn.addEventListener('click', clearGrid);
