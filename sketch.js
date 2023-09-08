1d0fd0be461753d596e1f6476e6376c6c20f8e46
Shreyaskr8 <skrravi08@gmail.com>
2023-09-08 14:45:07 +0530

    add a toggle grid lines button

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
-let gridSize = prompt("Enter grid size");
-const containerDiv = document.querySelector('.grid-container');
-// console.log(containerDiv);
 let gridDiv;
-function fillGrids()
+let mouseDown = false;
+let isRandomRGB = false;
+let chosenColor = 'rgb(0, 0, 0)';
+const containerDiv = document.querySelector('.grid-container');
+const gridSizeBtn = document.querySelector('.grid-size-button');
+const randomColorBtn = document.querySelector('.random-rgb-button');
+const toggleGridBtn = document.querySelector('.toggle-grid-button');
+const clearBtn = document.querySelector('.clear-button');
+
+function fillGrids(gridSize = 16)
 {
     containerDiv.innerHTML = ''; // clears previous divs, if any
     for(let i = 0; i < gridSize*gridSize; i++)
@@ -15,14 +21,80 @@ function fillGrids()
     }
 }
 
-fillGrids();
-const gridSizeBtn = document.querySelector('.grid-size-button');
-gridSizeBtn.addEventListener('click', () => {
-    gridSize = prompt("Enter grid size");
-    fillGrids();
-})
-
-containerDiv.addEventListener('mouseover', (e) => {
+function applyColorOnClick(e)
+{
+    if(e.type === 'mouseover' && !mouseDown)
+    {
+        return;
+    }
     const target = e.target;
-    target.style.backgroundColor = 'black';
-})
+    target.style.backgroundColor = chosenColor; 
+}
+
+function applyColorOnHover(e)
+{
+    if(isRandomRGB)
+    {
+        setRandomColor();
+    }
+    const target = e.target;
+    target.style.backgroundColor = chosenColor; 
+}
+
+function setRandomColor()
+{
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    chosenColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function toggleGridLines(gridLines)
+{
+    containerDiv.classList.toggle('remove-grid-border');
+    
+    containerDiv.childNodes.forEach(gridDiv => {
+        gridDiv.classList.toggle('remove-grid-border');
+    });
+}
+
+function clearGrid()
+{
+    containerDiv.childNodes.forEach(gridDiv => {
+        gridDiv.style.backgroundColor = 'white'
+    });
+}
+
+fillGrids();
+
+gridSizeBtn.addEventListener('click', () => {
+    do{
+        var gridSize = prompt("Enter the grid size");
+    }while(gridSize > 100 || gridSize <= 0);
+    fillGrids(gridSize);
+});
+
+// containerDiv.addEventListener('mouseover', applyColorOnClick);
+// containerDiv.addEventListener('mousedown', applyColorOnClick);
+containerDiv.addEventListener('mouseover', applyColorOnHover);
+
+document.body.onmousedown =  () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
+randomColorBtn.addEventListener('click', () => {
+    if(!isRandomRGB)
+    {
+        isRandomRGB = true;
+        randomColorBtn.textContent = 'Black Color';
+    }
+    else 
+    {
+        isRandomRGB = false;
+        chosenColor = 'rgb(0, 0, 0)';
+        randomColorBtn.textContent = 'Random RGB';
+    }
+});
+
+toggleGridBtn.addEventListener('click', toggleGridLines);
+
+clearBtn.addEventListener('click', clearGrid);
